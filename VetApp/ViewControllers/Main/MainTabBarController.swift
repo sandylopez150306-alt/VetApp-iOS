c5da7b0aa6564190f2b961320e50ad522930fb7e
Danner Loyola <loyoladanner@gmail.com>
2026-04-24 19:49:26 -0500

    Revert "Avance Actualizado 2.0 implementación completa perfil y home"
    This reverts commit 6d1769ce96f0596a3f8a5be6e9cbba6c37df233e.

--- VetApp/ViewControllers/Main/MainTabBarController.swift
+++ VetApp/ViewControllers/Main/MainTabBarController.swift
@@ -3,49 +3,17 @@ import UIKit
 class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        tabBar.tintColor = UIColor(red: 0.06, green: 0.53, blue: 0.49, alpha: 1)
-        tabBar.backgroundColor = .systemBackground
+        tabBar.tintColor = .systemTeal
         
-        // 1. Inicio
-        let home = UINavigationController(rootViewController: HomeViewController())
-        home.tabBarItem = UITabBarItem(
-            title: "Inicio",
-            image: UIImage(systemName: "house"),
-            selectedImage: UIImage(systemName: "house.fill")
-        )
-        
-        // 2. Mascotas
         let mascotas = UINavigationController(rootViewController: MascotasViewController())
-        mascotas.tabBarItem = UITabBarItem(
-            title: "Mascotas",
-            image: UIImage(systemName: "pawprint"),
-            selectedImage: UIImage(systemName: "pawprint.fill")
-        )
+        mascotas.tabBarItem = UITabBarItem(title: "Mascotas", image: UIImage(systemName: "pawprint.fill"), tag: 0)
         
-        // 3. Citas
         let citas = UINavigationController(rootViewController: CitasViewController())
-        citas.tabBarItem = UITabBarItem(
-            title: "Citas",
-            image: UIImage(systemName: "calendar"),
-            selectedImage: UIImage(systemName: "calendar.badge.clock")
-        )
+        citas.tabBarItem = UITabBarItem(title: "Citas", image: UIImage(systemName: "calendar"), tag: 1)
         
-        // 4. Clínica (Mapa)
         let mapa = UINavigationController(rootViewController: MapaViewController())
-        mapa.tabBarItem = UITabBarItem(
-            title: "Clínica",
-            image: UIImage(systemName: "mappin.and.ellipse"),
-            selectedImage: UIImage(systemName: "mappin.and.ellipse")
-        )
+        mapa.tabBarItem = UITabBarItem(title: "Clínica", image: UIImage(systemName: "mappin.and.ellipse"), tag: 2)
         
-        // 5. Perfil
-        let perfil = UINavigationController(rootViewController: PerfilViewController())
-        perfil.tabBarItem = UITabBarItem(
-            title: "Perfil",
-            image: UIImage(systemName: "person"),
-            selectedImage: UIImage(systemName: "person.fill")
-        )
-        
-        viewControllers = [home, mascotas, citas, mapa, perfil]
+        viewControllers = [mascotas, citas, mapa]
     }
 }
